144a3b06befcb78acde11f9b8ec24f567e2fa583
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-29 13:01:56 +0100

    Create about component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = function About() {
+  return (
+    <section className="about_container">
+      <article>Hola mi nombre es Marc Martín Barrios. Tengo 32 años </article>
+    </section>
+  );
+};
+
+export default About;
