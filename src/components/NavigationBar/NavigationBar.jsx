95cb4dbc24b45c40a9283256a0aeafa95bb12096
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-29 13:02:33 +0100

    Create navigation bar component

---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = function NavBar() {
+  return (
+    <header>
+      <nav className="header_navigation">
+        <ul className="header_navigation_list">
+          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Home</a></li>
+          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">About</a></li>
+          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">CV</a></li>
+          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
