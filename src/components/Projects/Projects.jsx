1a4ea47c8bc40b547ccd0d790e15f115f72f3b02
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-30 11:44:47 +0100

    Change name of Home component to Project component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = function projects() {
+  return (
+    <div>
+      <h2>List of projects</h2>
+      <p>These are some of the projects I made, and some that I'm currently working on:</p>
+    </div>
+  );
+};
+
+export default Projects;
