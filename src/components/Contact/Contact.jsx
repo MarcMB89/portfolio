2e8775747c24e84add2d3a4587b1a608d295dd3b
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-30 11:43:30 +0100

    Add contact info on contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
-const Contact = function Contact() {
+const Contact = function contact() {
   return (
-    <div>
-      <p>Contact info</p>
-    </div>
+    <footer>
+      <h2>Contact info</h2>
+      <p>Email: </p>
+      <address>
+        <a
+          href="marcmartinbarrios@gmail.com"
+          className="footer_links"
+        >
+          marcmartinbarrios@gmail.com
+        </a>
+      </address>
+      <p>GitHub: </p>
+      <address>
+        <a
+          href="https://github.com/MarcMB89"
+          className="footer_links"
+        >
+          https://github.com/MarcMB89
+        </a>
+      </address>
+      <p>Linkedin: </p>
+      <address>
+        <a
+          href="https://www.linkedin.com/in/marc-mart%C3%ADn-barrios/"
+          className="footer_links"
+        >
+          https://www.linkedin.com/in/marc-mart%C3%ADn-barrios/
+        </a>
+      </address>
+    </footer>
   );
 };
 
