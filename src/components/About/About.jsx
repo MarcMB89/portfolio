9a3579d7735abb08d0d82598ffb088ab48485eba
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-30 11:43:13 +0100

    Add 3 sections on About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,9 +2,37 @@ import React from 'react';
 
 const About = function About() {
   return (
-    <section className="about_container">
-      <article>About</article>
-    </section>
+    <div className="about_container">
+      <section className="about_title">
+        <h1>Marc Martín Barrios</h1>
+        <h2>About</h2>
+      </section>
+      <section className="about_first-section">
+        <img
+          className="about_profile-photo"
+          src="https://avatars.githubusercontent.com/u/78495996?v=4"
+          alt="Marc Martín Barrios profile pic"
+        />
+        <article className="about_first-section_description">
+          My name is Marc, I'm 32 years old and a passionate for software development
+        </article>
+      </section>
+      <section className="about_second-section">
+        <article className="about_second-section_interests">
+          I'm specially interested in Mobile App and web development
+        </article>
+        <img
+          className="about_plantdroid-gif"
+          src="https://user-images.githubusercontent.com/78495996/141802054-7074be9f-db4c-4dcb-88e7-0db344eead0c.gif"
+          alt="Plantdroid App gif"
+        />
+      </section>
+      <section className="about_third-section">
+        <article>
+          I specialize in Javascript, React and React-Native. I use MERN technologies.
+        </article>
+      </section>
+    </div>
   );
 };
 
