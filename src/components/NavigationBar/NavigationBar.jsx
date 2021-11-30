812f28894c6c7b6a769f78e6e11b1d8d18af5785
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-30 11:44:12 +0100

    Modify name of a list item to projects

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -5,8 +5,8 @@ const NavBar = function NavBar() {
     <header>
       <nav className="header_navigation">
         <ul className="header_navigation_list">
-          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Home</a></li>
           <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">About</a></li>
+          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Projects</a></li>
           <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">CV</a></li>
           <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Contact</a></li>
         </ul>
