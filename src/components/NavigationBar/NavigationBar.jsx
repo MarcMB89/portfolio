2e08b818b4278086d9e120c75a83baf7576d5ec2
Marc Martin <marcmartinbarrios@gmail.com>
2021-12-15 15:34:33 +0100

    Add h1 to navigation bar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -4,6 +4,7 @@ const NavBar = function NavBar() {
   return (
     <header>
       <nav className="header_navigation">
+        <h1 className="header_navigation_title">MARC MARTÍN BARRIOS</h1>
         <ul className="header_navigation_list">
           <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">About</a></li>
           <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Projects</a></li>
