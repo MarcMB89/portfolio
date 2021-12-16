5020ce844acbae5ceb9c0bcac690696dfee998e7
Marc Martin <marcmartinbarrios@gmail.com>
2021-12-16 17:42:09 +0100

    Create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = function footer() {
+  return (
+    <footer>
+      <h6><a href="https://github.com/MarcMB89/portfolio">Designed and built by Marc Martín 2021</a></h6>
+    </footer>
+  );
+};
+
+export default Footer;
