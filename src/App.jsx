9ce2c0fef34587f6635a27dca49e6ba8cb77aec1
Marc Martin <marcmartinbarrios@gmail.com>
2021-12-16 17:39:27 +0100

    Add footer component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import NavBar from './components/NavigationBar/NavigationBar';
+import NavBar from './components/Header/NavigationBar';
 import CV from './components/CV/Curriculum';
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
-import Projects from './components/Projects/Projects';
+import Projects from './components/Project-List/Projects';
+import Footer from './components/Footer/Footer';
 
 const App = function App() {
   return (
     <div className="App">
       <NavBar />
-      <h2 className="construction">UNDER CONSTRUCTION</h2>
       <About id="about" />
       <Projects id="projects" />
       <CV id="cv" />
       <Contact id="contact" />
+      <Footer is="footer" />
     </div>
   );
 };
