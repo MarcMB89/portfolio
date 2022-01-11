3c3bf23d924587484f5f1586ac3c79e7c4802de4
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-11 17:09:16 +0100

    Add navigation links on header

--- src/components/Header/NavigationBar.jsx
+++ src/components/Header/NavigationBar.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './navigationBarStyle.css';
 
-const NavBar = function NavBar() {
+const Header = function navBar() {
   return (
-    <header>
-      <nav className="header_navigation">
-        <h1 className="header_navigation_title">MARC MARTÍN BARRIOS</h1>
-        <ul className="header_navigation_list">
-          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">About</a></li>
-          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Projects</a></li>
-          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">CV</a></li>
-          <li className="header_navigation_list_item"><a href="http://localhost:3000/portfolio">Contact</a></li>
+    <header className="header__desktop">
+      <nav className="header__navigation">
+        <h1 className="header__navigation-title">
+          <a href="http://localhost:3000/portfolio">
+            MARC MARTÍN BARRIOS
+          </a>
+        </h1>
+        <ul className="header__navigation-list">
+          <Link
+            to="/about"
+            key={1}
+          >
+            About
+          </Link>
+          <Link
+            to="/projects"
+            key={1}
+          >
+            Projects
+          </Link>
+          <Link
+            to="/cv"
+            key={1}
+          >
+            CV
+          </Link>
+          <Link
+            to="/contact"
+            key={1}
+          >
+            contact
+          </Link>
         </ul>
       </nav>
     </header>
   );
 };
 
-export default NavBar;
+export default Header;
