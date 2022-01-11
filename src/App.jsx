63ada1fdbad5c3b429365a9f00baa49b5af01939
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-11 17:32:45 +0100

    Add not-found route on App

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Projects from './components/Project-List/Projects';
 import CV from './components/CV/Curriculum';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import NotFound from './components/Not-found/Not-found';
 
 const App = function App() {
   return (
@@ -16,11 +17,11 @@ const App = function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" exact element={<App />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/cv" element={<CV />} />
           <Route path="/contact" element={<Contact />} />
+          <Route element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
