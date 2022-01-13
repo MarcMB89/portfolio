c57d20bcce21f748a347546bfebeee7801b42583
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-13 16:05:09 +0100

    Add home and GoBackButtom components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
+
 import {
-  BrowserRouter as Router, Route, Routes
+  BrowserRouter as Router
 } from 'react-router-dom';
 
 import Header from './components/Header/NavigationBar';
+import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Project-List/Projects';
 import CV from './components/CV/Curriculum';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import GoBackButton from './components/ScrollBackToTop/ScrollBack';
 
 const App = function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/cv" element={<CV />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    <Router>
+      <Header />
+      <Home />
+      <About />
+      <Projects />
+      <CV />
+      <Contact />
+      <GoBackButton />
+      <Footer />
+    </Router>
   );
 };
 
