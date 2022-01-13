f32a12a6416c4da628db144c881bff5cd503c138
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-13 17:46:24 +0100

    Add goBackButton logic

--- src/components/ScrollBackToTop/ScrollBack.jsx
+++ src/components/ScrollBackToTop/ScrollBack.jsx
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import { Fab } from '@mui/material';
 import './scrollBackStyle.css';
 
 const GoBackButton = function button() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+  const handleScroll = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', handleVisibility);
+    };
+  });
+
   return (
     <section>
-      <Link
-        className="arrow-wrapper"
-        smooth
-        to="#home"
-        key={0}
-      >
-        <Fab
-          fontSize="medium"
-          color="primary"
-          style={{
-            backgroundColor: '#407A79',
-            opacity: 0.7
-          }}
+      {isVisible
+        && (
+        <Link
+          onClick={handleScroll}
+          className="arrow-wrapper"
+          smooth
+          to="#home"
+          key={0}
         >
-          <ArrowUpwardIcon
+          <Fab
+            fontSize="medium"
+            color="primary"
             style={{
-              fontSize: '40px'
+              backgroundColor: '#407A79',
+              opacity: 0.7
             }}
-            smooth
-            to="#home"
-          />
-        </Fab>
-      </Link>
+          >
+            <ArrowUpwardIcon
+              style={{
+                fontSize: '40px'
+              }}
+              smooth
+              to="#home"
+            />
+          </Fab>
+        </Link>
+        )}
     </section>
   );
 };
