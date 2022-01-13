355a84983faef03de4d2e77cca339e4aab73b991
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-13 16:06:46 +0100

    Create Home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = function projects() {
+  return (
+    <div>
+      <p
+        className="home__container"
+        id="home"
+      />
+    </div>
+  );
+};
+
+export default Home;
