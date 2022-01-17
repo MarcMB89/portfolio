5bdec6ee7b19013f844ff5c4801347438ed07c90
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-17 10:25:50 +0100

    Delete image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,13 +3,15 @@ import './aboutStyle.css';
 
 const About = function About() {
   return (
-    <div className="about__container">
+    <section className="about__container">
+      <h2>Under construction</h2>
       <h2
         className="about__title"
         id="about"
       >
         About
       </h2>
+
       <article className="about_first-section_description">
         My name is Marc, I'm 32 years old and a passionate for software development
       </article>
@@ -18,18 +20,15 @@ const About = function About() {
         <article className="about_second-section_interests">
           I'm specially interested in Mobile App and web development
         </article>
-        <img
-          className="about_plantdroid-gif"
-          src="https://user-images.githubusercontent.com/78495996/141802054-7074be9f-db4c-4dcb-88e7-0db344eead0c.gif"
-          alt="Plantdroid App gif"
-        />
       </section>
+
       <section className="about_third-section">
         <article>
           I specialize in Javascript, React and React-Native. I use MERN technologies.
         </article>
       </section>
-    </div>
+
+    </section>
   );
 };
 
