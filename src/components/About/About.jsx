d81f4cc6a52b79173cae9a48400dabc547cd8e53
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-19 13:35:39 +0100

    Create and style basic About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './aboutStyle.css';
+
+const About = function about() {
+  return (
+    <section className="about__container">
+      <h2 className="about__title" id="about">About</h2>
+    </section>
+  );
+};
+
+export default About;
