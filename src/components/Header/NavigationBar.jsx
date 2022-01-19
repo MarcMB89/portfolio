11119f26a18569475de23648cc673003446ae1c0
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-19 13:37:15 +0100

    Add js logic for navbar to dissapear

--- src/components/Header/NavigationBar.jsx
+++ src/components/Header/NavigationBar.jsx
@@ -3,17 +3,27 @@ import { HashLink as Link } from 'react-router-hash-link';
 import './navigationBarStyle.css';
 
 const Header = function navBar() {
+  let previousScrollPosition = window.pageYOffset;
+  window.onscroll = function hideNavbar() {
+    const currentScrollPosition = window.pageYOffset;
+    if (previousScrollPosition > currentScrollPosition) {
+      document.getElementById('navbar').style.top = '0';
+    } else {
+      document.getElementById('navbar').style.top = '-50px';
+    }
+    previousScrollPosition = currentScrollPosition;
+  };
   return (
-    <header className="header__desktop">
-      <nav className="header__navigation">
-        <h1 className="header__navigation-title">
-          UNDER CONSTRUCTION
-        </h1>
+    <header>
+      <nav className="header__navigation-desktop" id="navbar">
         <ul className="header__navigation-list">
+          <h1 className="header__navigation-title">
+            UNDER CONSTRUCTION
+          </h1>
           <Link
             className="header__navigation-list-item"
             smooth
-            to="#home"
+            to="#about"
             key={1}
           >
             About
