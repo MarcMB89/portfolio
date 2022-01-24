0afc0771a9f22b932109064163b5441a2cad2422
Marc Martin <marcmartinbarrios@gmail.com>
2022-01-24 12:04:34 +0100

    Fix margins of home component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,24 +8,19 @@ const About = function about() {
       <article className="about__description-article">
         <p>
           Hi, my name is Marc, I'm a freelance web developer currently based in Barcelona, Spain.
-          <br />
-          <br />
-          After many years exploring different careers, I came across web development around 2019
-          and I realized that is the job I always wanted.
-          <br />
-          <br />
+          <p>
+            After many years exploring different careers, I came across web development around 2019
+            and I realized that is the job I always wanted.
+          </p>
           Since then I started studying my myself, reading documentation, practicing,
           taking online courses
           until I decided to take a step further and I joint Skylab Coders Academy (now
           <a href="https://isdicoders.com/ca/full-time-format-bootcamp-barcelona_5278"> ISDI Coders</a>
           ), one of the
-          {' '}
           <a href="https://www.switchup.org/rankings/best-bootcamps-barcelona" blank>best bootcamps in Barcelona (according to SwitchUp).</a>
-          <br />
-          <br />
-          These are some of my favourite technologies that I've been using recently:
-          <br />
-          <br />
+          <p>
+            These are some of my favourite technologies that I've been using recently:
+          </p>
           <ul>
             <li>Javascript</li>
             <li>HTML5</li>
@@ -36,8 +31,9 @@ const About = function about() {
             <li>React-Native</li>
             <li>Redux</li>
           </ul>
-          <br />
-          Apart from those, I also use Figma for the design phase of any project
+          <p>
+            Apart from those, I also use Figma for the design phase of any project
+          </p>
         </p>
       </article>
     </section>
